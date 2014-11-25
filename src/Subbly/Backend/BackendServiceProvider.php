1851654337ace142fecbaa2bd9395d4d8042b514
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-11-25 21:23:53 +0100

    Rename the provider class

--- src/Subbly/Backend/BackendServiceProvider.php
+++ src/Subbly/Backend/BackendServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class BackendServiceProvider extends ServiceProvider {
+class SubblyBackendServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
