87a3025131f1deeccefcac679da12fee9e2f2838
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-11-28 02:43:15 +0100

    Cleanup

--- src/Subbly/Backend/SubblyBackendServiceProvider.php
+++ src/Subbly/Backend/SubblyBackendServiceProvider.php
@@ -1,46 +1,48 @@
-<?php namespace Subbly\Backend;
+<?php
+
+namespace Subbly\Backend;
 
 use Illuminate\Support\ServiceProvider;
 
 class SubblyBackendServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('subbly/backend');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('subbly/backend');
 
-    include __DIR__.'/../../routes.php';
-	}
+        include __DIR__.'/../../routes.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
