36594299de3360b0ac1d3082f450768f49d95854
michael-lefebvre <michael@scenedata.com>
2015-02-02 20:41:47 +0100

    clear product required field

--- public/js/models/product.js
+++ public/js/models/product.js
@@ -76,27 +76,11 @@ Components.Subbly.Model.Product = SubblyModel.extend(
             , rules:   'required'
           }
         , {
-              name:    'sku'
-            , rules:   'required'
-          }
-        , {
               name:    'name'
             , rules:   'required'
           }
         , {
-              name:    'description'
-            , rules:   'required'
-          }
-        , {
               name:    'price'
-            , rules:   'required|numeric'
-          }
-        , {
-              name:    'sale_place'
-            , rules:   'numeric'
-          }
-        , {
-              name:    'quantity'
             , rules:   'required|numeric'
           }
       ]
